dc2b9ead88a65106bc1cdc29eac2b79126848c56
pschabinska@gmail.com <paula.schabinska@croonwolterendros.nl>
2021-01-05 17:22:42 +0100

    New menu and submenu

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Toggle mobile menu */
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+
+function toggleMenu() {
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+toggle.addEventListener("click", toggleMenu, false);
+
